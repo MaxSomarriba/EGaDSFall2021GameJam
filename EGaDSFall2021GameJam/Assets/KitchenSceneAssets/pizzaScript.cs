9767bf45724aec3f6294bfad57c005e6468bb54a
MaxSomarriba <49109430+MaxSomarriba@users.noreply.github.com>
2021-09-25 14:49:28 -0500

    Restaurant Side Commit,
    no art assets implement*

---
+++ EGaDSFall2021GameJam/Assets/KitchenSceneAssets/pizzaScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pizzaScript : MonoBehaviour
+{
+    [SerializeField] private bool triggerActive = false;
+    private GameObject playerScriptsUsedForFoodReference;
+    private holding whatIsPlayerHolding;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScriptsUsedForFoodReference = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        whatIsPlayerHolding = playerScriptsUsedForFoodReference.GetComponent<playerScripts>().getCurrentHolding();
+        //For trigger
+        if (triggerActive && Input.GetKeyDown(KeyCode.Space) && whatIsPlayerHolding == holding.Nothing)
+        {
+            //Debug.Log(playerScriptsUsedForFoodReference.GetComponent<playerScripts>().getCurrentHolding().GetType());
+            pizzaPickUp();
+        }
+
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggerActive = true;
+
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggerActive = false;
+        }
+    }
+    public void pizzaPickUp()
+    {
+        playerScriptsUsedForFoodReference.GetComponent<playerScripts>().pickUpPizza();
+        Destroy(gameObject);
+    }
+}
---
+++ EGaDSFall2021GameJam/Assets/KitchenSceneAssets/playerScripts.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Holding
+public enum holding
+{
+    Nothing,
+    Pizza,
+    Burger,
+    Salad
+}
+public class playerScripts : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+
+    public float runSpeed = 20.0f;
+
+    
+    
+    public holding isHolding;
+    //ordering
+    public int pizza = 0;
+    public int burger = 0;
+    public int salad = 0;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+    public void addPizza()
+    {
+        pizza++;
+    }
+    public void addBurger()
+    {
+        burger++;
+    }
+    public void addSalad()
+    {
+        salad++;
+    }
+    public int getPizza()
+    {
+        return pizza;
+    }
+    public int getBurger()
+    {
+        return burger;
+    }
+    public int getSalad()
+    {
+        return salad;
+    }
+    public void clearOrders()
+    {
+        pizza = 0;
+        burger = 0;
+        salad = 0;
+    }
+    public holding getCurrentHolding()
+    {
+        return isHolding;
+    }
+    public void resetHolding()
+    {
+        isHolding = holding.Nothing;
+    }
+    public void pickUpPizza()
+    {
+        isHolding = holding.Pizza;
+    }
+    public void pickUpBurger()
+    {
+        isHolding = holding.Burger;
+    }
+    public void pickUpSalad()
+    {
+        isHolding = holding.Salad;
+    }
+}
