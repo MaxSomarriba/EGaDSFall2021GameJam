e2eeb0e69a3c14718804c6f06befebcc6b3d6250
Ethan Dahlstrom <ethandahlstrom@gmail.com>
2021-09-25 19:00:23 -0500

    Prepare for merge

--- EGaDSFall2021GameJam/Assets/TransitionAssets/OfficeEntranceTrigger.cs
+++ EGaDSFall2021GameJam/Assets/TransitionAssets/OfficeEntranceTrigger.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class OfficeEntranceTrigger : MonoBehaviour
 {
+    public static bool isTriggered;
+
     void OnTriggerEnter2D(Collider2D ChangeScene) {
+        isTriggered = true;
         Debug.Log("Triggered the office collider");
-        // KitchenEntranceTrigger.LoadScene("");
     }
 }
---
+++ EGaDSFall2021GameJam/Assets/TransitionAssets/SceneLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    AsyncOperation asyncLoad;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (KitchenEntranceTrigger.isTriggered) {
+            StartCoroutine(LoadYourAsyncScene(0));
+        } else if (OfficeEntranceTrigger.isTriggered) {
+            StartCoroutine(LoadYourAsyncScene(1));
+        }
+    }
+
+     IEnumerator LoadYourAsyncScene(int scene)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        if (scene == 1) {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("OfficeScene");
+        } else {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("restaurantScene");
+        }
+        
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
